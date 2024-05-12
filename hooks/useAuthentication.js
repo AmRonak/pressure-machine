00348015db94f7058a87bebb65bbb00fb0a1109b
Ronak Panchal <Ronakp540@outlook.com>
2024-05-12 21:53:46 +0530

    removed authentication temporary

--- hooks/useAuthentication.js
+++ hooks/useAuthentication.js
@@ -3,23 +3,23 @@ import { redirect, usePathname } from "next/navigation";
 const { useLayoutEffect, useState } = require("react");
 
 const useAuthentication = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const pathname = usePathname();
+  const [isLoading, setIsLoading] = useState(false);
+  // const pathname = usePathname();
 
 
-  useLayoutEffect(() => {
-    const { isAuthenticated } = JSON.parse(localStorage.getItem('auth'));
-    if(pathname === '/' || pathname === '/login') {
-      if(isAuthenticated){
-        redirect("/dashboard")
-      }
-    } else {
-      if(!isAuthenticated){
-        redirect("/login")
-      }
-    }
-    setIsLoading(false)
-  }, [pathname])
+  // useLayoutEffect(() => {
+  //   const { isAuthenticated } = localStorage.getItem('auth');
+  //   if(pathname === '/' || pathname === '/login') {
+  //     if(isAuthenticated){
+  //       redirect("/dashboard")
+  //     }
+  //   } else {
+  //     if(!isAuthenticated){
+  //       redirect("/login")
+  //     }
+  //   }
+  //   setIsLoading(false)
+  // }, [pathname])
 
   return isLoading;
 };
