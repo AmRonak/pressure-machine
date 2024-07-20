254d17d8dc1c346e1b1885ad610f419ab00073a1
Ronak Panchal <Ronakp540@outlook.com>
2024-07-21 00:25:43 +0530

    feat(date_time): added live date time

--- components/buttons/Navigation.jsx
+++ components/buttons/Navigation.jsx
@@ -1,26 +1,32 @@
 'use client';
 import Link from "next/link";
 import Image from "next/image";
-import moment from "moment";
 import { useAuthSelector } from "@/redux/slices/authSlice";
-import { useState } from "react";
-
-const currentDateTime = {
-  date: moment().format('DD - MM - YYYY'),
-  time: moment().format('HH:mm:ss')
-}
+import Clock from 'react-live-clock';
 
 const Navigation = () => {
   const {isAuthenticated, userDetail} = useAuthSelector();
-  const [dateTime,setDateTime] = useState(currentDateTime);
 
   return (
     <div className="flex flex-col justify-center fixed top-10 right-10">
       <div>
         <p className="text-end pr-4">
-          <span className="text-xl text-slate-400">{dateTime.date}</span>
+          <span>
+            <Clock
+              format={'DD-MM-YYYY'}
+              timezone={'locale'}
+              className="text-slate-400 text-xl"
+            />
+          </span>
           <span className="text-2xl text-slate-400"> | </span>
-          <span className="text-2xl font-bold text-slate-400">{dateTime.time}</span>
+          <span className="text-2xl font-bold text-slate-400">
+            <Clock
+            format={'HH:mm:ss'}
+            ticking={true}
+            timezone={'locale'}
+            className="text-slate-400"
+          />
+          </span>
         </p>
       </div>
       <div className="flex justify-end">
@@ -44,7 +50,7 @@ const Navigation = () => {
                   alt="user profile icon image"
                   className="hover:cursor-pointer"
                 />
-                <div className="opacity-0 w-auto bg-primary text-white text-center text-balance text-xs rounded-lg py-2 px-4 absolute z-10 group-hover:opacity-100 pointer-events-none top-20">
+                <div className="ml-2 opacity-0 w-auto bg-primary text-white text-center text-balance text-xs rounded-lg py-2 px-4 absolute z-10 group-hover:opacity-100 pointer-events-none top-20">
                   {userDetail?.username}
                 </div>
               </div>
